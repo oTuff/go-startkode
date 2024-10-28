d48c4a8414295134c7126355282834f5a6fb2b21
Oscar Tuff <90379013+oTuff@users.noreply.github.com>
2024-10-28 16:22:11 +0100

    Migration with test data

---
+++ db/migrations/20241028150239_instert_data.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+INSERT INTO todo (id, title, text, isCompleted, category, deadline)
+    VALUES (1, 'Task 1', 'This is the first task', FALSE, 'Work', '2023-12-31'),
+    (2, 'Task 2', 'This is the second task', TRUE, 'Personal', '2023-11-30'),
+    (3, 'Task 3', 'This is the third task', FALSE, 'Work', '2023-12-01');
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DELETE FROM todo
+WHERE id IN (1, 2, 3);
+
+-- +goose StatementEnd
