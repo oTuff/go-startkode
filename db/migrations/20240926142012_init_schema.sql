4c2f69db5f65a7eb7127a7ed1f74876dda642c60
Oscar Tuff <90379013+oTuff@users.noreply.github.com>
2024-11-02 20:31:05 +0100

    Add missing goose comment

--- db/migrations/20240926142012_init_schema.sql
+++ db/migrations/20240926142012_init_schema.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE todo(
+CREATE TABLE todo (
     id SERIAL8 PRIMARY KEY,
     title varchar(255) NOT NULL,
     text STRING NOT NULL,
@@ -14,3 +14,4 @@ CREATE TABLE todo(
 -- +goose StatementBegin
 DROP TABLE todo;
 
+-- +goose StatementEnd
